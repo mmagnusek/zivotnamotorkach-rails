4a8d32cd47c0175c08bc30071e467025c1b06e25
Martin Magnusek <magnusekm@gmail.com>
2014-12-22 14:54:45 +0100

    Update gems, remove capistrano

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :stage,     :production
-set :rails_env, 'production'
-set :deploy_to, '/var/www/zivotnamotorkach_cz'
-
-server '81.0.240.83',
-       user: 'zivotnamotorkach_cz',
-       roles: %w(web app db)
--- config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-set :stage,     :staging
-set :deploy_to, '/var/www/zivotnamotorkach-rails_webapps_cz'
-set :rails_env, 'staging'
-
-server 'REPLACE_ME',
-        user: 'zivotnamotorkach-rails_webapps_cz',
-        roles: %w(web app db)
-
