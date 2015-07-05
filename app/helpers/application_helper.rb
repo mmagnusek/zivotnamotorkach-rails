17e5bd16c14f1958177fd024d5b3200379388d0b
Martin Magnusek <magnusekm@gmail.com>
2015-07-05 13:41:17 +0200

    Add blog posts into map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,16 @@ module ApplicationHelper
     service = URI.parse(url).hostname.gsub(/www\./i, '')
     link_to service.capitalize, url, target: '_blank'
   end
+
+  def blog_post_markers(trip)
+    trip.blog_posts.map do |blog_post|
+      next if blog_post.location.blank?
+      text = [
+        blog_post.created_at.strftime('%d.%m.%Y'),
+        blog_post.title,
+        link_to('Článek', blog_post_path(blog_post))
+      ].join('<br>')
+      blog_post.location.slice('lat', 'lng').merge(text: text.html_safe)
+    end.compact.to_json
+  end
 end
